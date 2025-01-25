ac071fe3f3ee595631e650e77f97cab83420da05
anonymous-574 <140256100+anonymous-574@users.noreply.github.com>
2025-01-25 20:47:57 +0530

    Update settings.py

--- train/settings.py
+++ train/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'TrainManagement',
         'USER': 'root',
-        'PASSWORD': '4YmfBTMu',
+        'PASSWORD': 'Add_Your_Password',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
