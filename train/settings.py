12332044bf12dd272843ce0257f40134f7d50b4d
anonymous-574 <140256100+anonymous-574@users.noreply.github.com>
2025-01-25 20:48:12 +0530

    Update settings.py

--- train/settings.py
+++ train/settings.py
@@ -87,21 +87,6 @@ DATABASES = {
     }
 }
 
-'''
-import os
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': '3306',
-    }
-}
-'''
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
